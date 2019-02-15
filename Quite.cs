619383654e3ec8fe287e406cc5d50bb2eeae5cdb
Odinsmodin <46110098+Odinsmodin@users.noreply.github.com>
2019-02-15 12:15:32 -0500

    Quite.cs
    The interpreter for the Quite programming language.

---
+++ Quite.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Quite {
+		
+public class Program  {
+			
+public static void Main(String[] args) {
+	
+string Qnput;
+Qnput = Console.ReadLine();
+	     	         
+switch(Qnput) {
+case "PrintV":
+string jnput = Console.ReadLine();
+Console.WriteLine(jnput);
+break;
+default:
+Console.WriteLine("Quite the contrary to what we were expecting, my good fellow!");
+break;
+case "@":
+break;
+} 
+         		
+}
+			
+}
+	
+}
