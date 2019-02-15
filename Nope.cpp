c9ce3b64ec21f610dc71db0fbf4e48fad532297e
Odinsmodin <46110098+Odinsmodin@users.noreply.github.com>
2019-02-15 12:17:22 -0500

    Nope.cpp
    The interpreter for the Nope. programming language.

---
+++ Nope.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+    
+  string input;
+  getline(cin,input);
+  
+    //for the lols     
+    if(input=="Oof") {
+    		cout << "Nope.";
+    } else {
+    	cout << "Nope.";
+    }
+    
+}    
