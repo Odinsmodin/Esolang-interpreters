ff9117ed874d7cf210b271448d72c54e714cdbb7
Odinsmodin <46110098+Odinsmodin@users.noreply.github.com>
2019-03-22 11:51:52 -0400

    Boredom Interpreter

---
+++ BOREDOM.pas
@@ -0,0 +1,18 @@
+program pronouncedfynight (input,output);
+uses crt;
+const
+number = 69;
+var
+call : string;
+cool : string;
+coal : string;
+begin
+readln(call);
+    if(call='<ACTV>')then
+        while number=69 do
+            begin
+                readln(cool);
+                readln(coal);
+            end;
+writeln(coal);
+end.
