11c2b9dbb336ef838894700a0687bb8439de7987
Odinsmodin <46110098+Odinsmodin@users.noreply.github.com>
2019-03-22 11:51:10 -0400

    The interpreter for Livefish, a derivative of Deadfish.

---
+++ Livefish.pas
@@ -0,0 +1,24 @@
+program Livefish (input,output);
+uses crt;
+var
+coke : integer;
+cook : char;
+crak : string;
+begin
+repeat
+coke := 0;
+readln(Cook);
+if(cook='i')then
+coke := coke + 1;
+if(cook='d')then
+coke := coke - 1;
+if(cook='s')then
+coke := sqr(coke);
+if(cook='o')then
+readln(crak);
+if(coke=256)then
+Coke := 0;
+if(coke<0)then
+coke := 0;
+until cook='h';
+end.
